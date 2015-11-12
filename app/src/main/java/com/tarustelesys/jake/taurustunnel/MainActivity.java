773b927bd70831560330d4951e49aca1beb38883
tarrenj <jake.tarren@gmail.com>
2015-11-12 16:46:47 -0500

    Begins threaded work
    Creates a new class within MainActivity to handle threading, and tests
    it's execution.

--- app/src/main/java/com/tarustelesys/jake/taurustunnel/MainActivity.java
+++ app/src/main/java/com/tarustelesys/jake/taurustunnel/MainActivity.java
@@ -3,13 +3,11 @@ package com.tarustelesys.jake.taurustunnel;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import java.io.IOException;
-import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,35 +17,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-//    public void ping_server(View view) {
-//        //String server = "192.168.1.1";
-//        String server = "127.0.0.1";
-//        int port = 1;
-//        try {
-//            //Socket sock = new Socket(server, port);
-//            Socket sock = new Socket("192.168.1.1", 1);
-//            sock.getOutputStream().write((byte) '\n');
-//            int ch = sock.getInputStream().read();
-//            if (ch == '\n') {
-//                // Server responded
-//            } else {
-//                // No response
-//            }
-//            sock.close();
-//        } catch (UnknownHostException e) {
-//            // Unknown host
-//            try {
-//                Thread.sleep(5);
-//            } catch (InterruptedException ex) {
-//            }
-//        } catch (IOException e) {
-//            // No I/O
-//            try {
-//                Thread.sleep(5);
-//            } catch (InterruptedException ex) {
-//            }
-//        }
-//    }
+    // A simple method to test to make sure my button is working..
+    public void test_button(View view) {
+        Log.d("Button test", "Button seems to be working fine...");
+    }
 
     public void ping_server(View view) {
         // Make sure we have a network connection
@@ -56,14 +29,25 @@ public class MainActivity extends AppCompatActivity {
         NetworkInfo networkInfo = ConMgr.getActiveNetworkInfo();
         if (networkInfo != null && networkInfo.isConnected()) {
             // Active network connection
-            Log.d("Network connection","Network connection is active");
+            Log.d("Network connection", "Network connection is active");
+            // Create another thread for network stuff
+            //new NetworkTask().execute(R.string.localhost);
+            new NetworkTask().execute("127.0.0.1");
+
         } else {
             // No active network connection
             Log.d("Network connection", "Network connection isn't active");
         }
     }
 
-    public void test_button(View view) {
-        Log.d("Button test", "Button seems to be working fine...");
+    // A new class for network stuff thread
+    private class NetworkTask extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected Void doInBackground(String... target) {
+            Log.d("Network Task", "Attempting to ping: " + target[0]);
+            return null;
+        }
     }
+
 }
